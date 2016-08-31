5bec411c1800753ab0b0c79fd432d14f15e7eb68
Spas Spasov <s.spasov_it@abv.bg>
2016-08-31 15:06:45 +0300

    Update
    Change visualisation

--- MVC-Blog/Controllers/HomeController.cs
+++ MVC-Blog/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace MVC_Blog.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
+
         {
             var db = new ApplicationDbContext();
             var post = db.Posts.OrderByDescending(p => p.Date).Take(3);
